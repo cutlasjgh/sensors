299c4d7368d7680810b2e8915865d020035ddf50
Ben Duvall <bmduvall10@gmail.com>
2017-01-30 18:25:33 -0500

    Add files via upload
    Updated code to detect 2 targets (however it can't detect 3?)

--- lidar_pixy_move.ino
+++ lidar_pixy_move.ino
@@ -1,9 +1,13 @@
+#include <SPI.h>
 #include <Pixy.h>
 #include <LIDARLite.h>
 #include <Wire.h>
 
 Pixy pixy;
 LIDARLite lidar;
+uint16_t blocks;
+
+
 #define X_CENTER ((PIXY_MAX_X-PIXY_MIN_X)/2)
 void setup() {
   Serial.begin(9600);
@@ -14,23 +18,37 @@ void setup() {
 }
 
 void loop() {
-  uint16_t blocks;
+  int dif;
+  blocks = -1;
   char buffer[100];
   blocks = pixy.getBlocks();
   // PIXY STUFF
-  int dif = X_CENTER - pixy.blocks[0].x;
   String dir;
-  if(dif < 0) {
-    sprintf(buffer, "^%s~%s~%s", "PIXY", "direction", "right");
-    Serial.println(buffer);
-  }else if (dif > 0) {
-    sprintf(buffer, "^%s~%s~%s", "PIXY", "direction", "left");
-    Serial.println(buffer);
+  int CenterOfBlocks;
+  Serial.println(blocks);
+  if(blocks == 1) {
+    CenterOfBlocks = pixy.blocks[0].x;
+  } else if(blocks == 2) {
+    CenterOfBlocks = (pixy.blocks[0].x + pixy.blocks[1].x) / 2;
+  }
+  if(blocks == 1 || blocks == 2){
+    dif = X_CENTER - CenterOfBlocks;
+    if(dif < -5) {
+      sprintf(buffer, "^%s~%s~%s", "PIXY", "direction", "right");
+      Serial.println(buffer);
+    } else if (dif > 5) {
+      sprintf(buffer, "^%s~%s~%s", "PIXY", "direction", "left");
+      Serial.println(buffer);
+    } else {
+      sprintf(buffer, "^%s~%s~%s", "PIXY", "direction", "straight");
+      Serial.println(buffer);
+   }
   }
 
+  delay(500);
   
 
-  //LIDAR STUFF
+ //LIDAR STUFF
   sprintf(buffer, "^%s~%s~%d", "LIDAR", "cm", lidar.distance());
   Serial.println(buffer);
 }
