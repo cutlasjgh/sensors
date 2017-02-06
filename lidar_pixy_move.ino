0e23405ab71a65464e6c15bec7b295669c7df095
Ben Duvall <bmduvall10@gmail.com>
2017-02-06 18:01:18 -0500

    Updated code with timer included
    added timer, increased bit rate, made it so that the code ran even when there are no blocks detected, etc.

--- lidar_pixy_move.ino
+++ lidar_pixy_move.ino
@@ -2,22 +2,16 @@
 #include <Pixy.h>
 #include <LIDARLite.h>
 #include <Wire.h>
+#include <Timer.h>
 
+#define X_CENTER ((PIXY_MAX_X-PIXY_MIN_X)/2)
 Pixy pixy;
 LIDARLite lidar;
 uint16_t blocks;
+boolean debug = false;
+Timer t;
 
-
-#define X_CENTER ((PIXY_MAX_X-PIXY_MIN_X)/2)
-void setup() {
-  Serial.begin(9600);
-  Serial.print("Starting...\n");
-  pixy.init();
-  lidar.begin(0, true);
-
-}
-
-void loop() {
+void no() {
   int dif;
   blocks = -1;
   char buffer[100];
@@ -26,36 +20,65 @@ void loop() {
   String dir;
   static int i = 0;
   int CenterOfBlocks;
+
   if (blocks) {
-    i++;
-    if (i % 30 == 0) {
-      //Serial.println("Block Count: " + String(blocks));
+    //Serial.println("Block Count: " + String(blocks));
 
 
-      if (blocks == 1) {
-        CenterOfBlocks = pixy.blocks[0].x;
-      } else if (blocks == 2) {
-        CenterOfBlocks = (pixy.blocks[0].x + pixy.blocks[1].x) / 2;
-      }
-      if (blocks == 1 || blocks == 2) {
-        dif = X_CENTER - CenterOfBlocks;
-        if (dif < -5) {
-          sprintf(buffer, "^%s~%s~%s", "PIXY", "direction", "right");
-          Serial.print(buffer);
-        } else if (dif > 5) {
-          sprintf(buffer, "^%s~%s~%s", "PIXY", "direction", "left");
-          Serial.print(buffer);
-        } else {
-          sprintf(buffer, "^%s~%s~%s", "PIXY", "direction", "straight");
-          Serial.print(buffer);
-        }
-
-      }
-      //LIDAR STUFF
-      sprintf(buffer, "^%s~%s~%d", "LIDAR", "cm", lidar.distance());
+    if (blocks == 1) {
+      CenterOfBlocks = pixy.blocks[0].x;
+    } else if (blocks == 2) {
+      CenterOfBlocks = (pixy.blocks[0].x + pixy.blocks[1].x) / 2;
+    } else {
+      CenterOfBlocks = 0;
+    }
+    //if (blocks == 1 || blocks == 2) {
+    dif = X_CENTER - CenterOfBlocks;
+    if (dif < -5) {
+      sprintf(buffer, "^%s~%s~%s~", "PIXY", "direction", "right");
+      Serial.print(buffer);
+    } else if (dif > 5) {
+      sprintf(buffer, "^%s~%s~%s~", "PIXY", "direction", "left");
+      Serial.print(buffer);
+    } else {
+      sprintf(buffer, "^%s~%s~%s~", "PIXY", "direction", "straight");
       Serial.print(buffer);
     }
-    
+    if (debug) {
+      Serial.println("");
+    }
+
+  }
+
+
+  //LIDAR STUFF
+  sprintf(buffer, "^%s~%s~%d~", "LIDAR", "cm", lidar.distance());
+  Serial.print(buffer);
+  if (debug) {
+    Serial.println("");
   }
 }
+void setup() {
+  Serial.begin(74880);
+  pixy.init();
+  if (debug) {
+    Serial.println("PIXY ONLINE");
+  }
+  lidar.begin(0, true);
+  if (debug) {
+    Serial.println("LIDAR ONLINE");
+  }
+  t.every(20, no);
+
+}
+
+void loop() {
+
+  t.update();
+
+}
+
+
+
+
 
